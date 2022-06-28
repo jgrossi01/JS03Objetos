dfc89269db00322630a0e7aa9f45cc3f36311513
jgrossi01 <107372992+jgrossi01@users.noreply.github.com>
2022-06-28 11:41:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ class Car {
 }
 
 const arrayCars = [];
-arrayCars.push(new Car (nextIndexOf(arrayCars),"etios", 2000));
-arrayCars.push(new Car (nextIndexOf(arrayCars),"corolla", 3000));
-arrayCars.push(new Car (nextIndexOf(arrayCars),"hilux", 4000)); 
+arrayCars.push(new Car (nextIndexOf(arrayCars),"Etios", 2000));
+arrayCars.push(new Car (nextIndexOf(arrayCars),"Corolla", 3000));
+arrayCars.push(new Car (nextIndexOf(arrayCars),"Hilux", 4000)); 
 
 console.log(arrayCars);
 
@@ -28,61 +28,80 @@ const arrayReservations = [];
 let keepBuying; 
 
 do {
-let reserveThis;
-let modelInput = prompt("Que modelo desea reservar?");
+    let reserveThis;
+    let modelInput = prompt("Que modelo desea reservar? Podemos ofrecerle los siguientes:\n" + availableCars());
 
-if (modelInput && isNaN(modelInput)){
-    modelInput = modelInput.toLowerCase();
-    reserveThis = arrayCars.find(model => model.name === modelInput);
-} else {
-    alert('No encontramos el modelo solicitado');
-}
+    if (modelInput && isNaN(modelInput)){
+        modelInput = modelInput.toLowerCase();
+        reserveThis = arrayCars.find(model => model.name.toLowerCase() === modelInput);
+        while(!reserveThis && keepBuying) {
+            keepBuying = confirm('No encontramos el modelo solicitado. Desea seguir comprando?')
+            if(keepBuying){
+                modelInput = prompt("Que modelo desea reservar?");
+                modelInput = modelInput.toLowerCase();
+                reserveThis = arrayCars.find(model => model.name.toLowerCase() === modelInput);
+            } else {
+                break;
+            }
+        }
+    }
 
-let quantityInput;
-if(reserveThis){
-    quantityInput = prompt(`Cuantos ${modelInput} precisa reservar?`);
-    while (!quantityInput || isNaN(quantityInput) || quantityInput < 1){
-        quantityInput = prompt(`Ingrese un número válido. Cuantos ${modelInput} precisa reservar?`);
-    } 
-} else {
-    break;
-}
+    let quantityInput;
+    if(reserveThis){
+        quantityInput = prompt(`Cuantos ${modelInput} precisa reservar?`);
+        while (!quantityInput || isNaN(quantityInput) || quantityInput < 1){
+            quantityInput = prompt(`Ingrese un número válido. Cuantos ${modelInput} precisa reservar?`);
+        } 
+    } else {
+        break;
+    }
 
-let daysInput;
-if(quantityInput){
-    daysInput = prompt(`Por cuantos días precisa los ${quantityInput} ${modelInput}?`);
-    while (!daysInput || isNaN(daysInput) || daysInput < 1){
-        daysInput = prompt(`Ingrese un número válido. Por cuantos días precisa los ${quantityInput} ${modelInput}?`);
-    } 
-} else {
-    break;
-}
+    let daysInput;
+    if(quantityInput){
+        daysInput = prompt(`Por cuantos días precisa los ${quantityInput} ${modelInput}?`);
+        while (!daysInput || isNaN(daysInput) || daysInput < 1){
+            daysInput = prompt(`Ingrese un número válido. Por cuantos días precisa los ${quantityInput} ${modelInput}?`);
+        } 
+    } else {
+        break;
+    }
 
-if(reserveThis && quantityInput && daysInput) {
-    
-    let name = reserveThis.name;
-    let dayprice = reserveThis.dayprice;
-    let total = Number(reserveThis.dayprice) * daysInput * quantityInput;
-    console.log(total);
-    arrayReservations.push(new Reservation (nextIndexOf(arrayReservations), name, quantityInput, daysInput, dayprice, total));
-    sum('total');
+    if(reserveThis && quantityInput && daysInput) {
+        
+        let name = reserveThis.name;
+        let dayprice = reserveThis.dayprice;
+        let total = Number(reserveThis.dayprice) * daysInput * quantityInput;
+        saveThis(name, quantityInput, daysInput, dayprice, total);   
 
-} else {
-    alert('Algo salio mal');
-}
+    } else {
+        alert('Algo salio mal');
+    }
 
-keepBuying = confirm('Quiere seguir comprando?')
-
+    keepBuying = confirm('Quiere seguir comprando?')
 } while(keepBuying);
 
 function nextIndexOf(array) {
     return array.length +1;
 };
 
-function sum (key){
-    let finaltotal = arrayReservations.reduce((a, b) => a + b[key], 0);
-    console.log(finaltotal);
+function availableCars() {
+    return arrayCars.map(u => u.name).join(`\n`);
 }
+
+function saveThis(name, quantityInput, daysInput, dayprice, total){
+    let id = nextIndexOf(arrayReservations);
+    arrayReservations.push(new Reservation (id, name, quantityInput, daysInput, dayprice, total));
+    console.log('Se agregó a tu carrito '+ quantityInput +' '+ name +' por '+ daysInput +' días. Total parcial: $'+ total);
+}
+
+if (arrayReservations.length > 1){
+    let finalqty = arrayReservations.reduce((a, b) => a + b['quantity'], 0);
+    let finaltotal = arrayReservations.reduce((a, b) => a + b['total'], 0);
+    console.log('Reservó correctamente '+finalqty+' vehiculos por un total final de $'+finaltotal);
+} else {
+    console.log('No realizó ninguna reserva.')
+}
+
 
 console.log(arrayReservations);
 
